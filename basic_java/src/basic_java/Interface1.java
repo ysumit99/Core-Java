bbeeea6e86e27f89276c0056b9bad82955664760
ysumit99 <ysumit99@gmail.com>
2018-10-15 22:47:26 +0530

    Interfaces done

---
+++ basic_java/src/basic_java/Interface1.java
@@ -0,0 +1,6 @@
+package basic_java;
+
+public interface Interface1 {
+	
+	int getInterestRate();
+}
---
+++ basic_java/src/basic_java/interface_driver.java
@@ -0,0 +1,11 @@
+package basic_java;
+
+public class interface_driver implements Interface1 {
+	
+	public int getInterestRate() {
+			return 0;
+	}
+}
+
+// class class extends
+//interface class implements
