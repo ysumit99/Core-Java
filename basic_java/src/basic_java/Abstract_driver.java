d5950d19664b9c6b3b9d6ea764f85a9ceab73fe2
ysumit99 <ysumit99@gmail.com>
2018-10-15 13:19:49 +0530

    Abstract classses and methods

---
+++ basic_java/src/basic_java/Abstract_driver.java
@@ -0,0 +1,15 @@
+package basic_java;
+
+public class Abstract_driver extends Test_abstract_method {
+
+	
+	//Abstract classes cannot be instantiated
+	//Implementing 'test' method is compulsory
+	 int test() {
+		 return 0;
+	 }
+
+}
+
+//Opposite of "abstract class" is "concrete" which is not required to be declared as concrete. 
+//It is by default concrete
---
+++ basic_java/src/basic_java/Test_abstract_method.java
@@ -0,0 +1,9 @@
+package basic_java;
+
+public abstract class Test_abstract_method  {
+	
+	
+	abstract int test();
+	
+	//Subclasses of this class must implement all the abstract methods
+}
